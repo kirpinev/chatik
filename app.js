3bfd9b89dc5c77d0ad2ce6c0fac710e6e67012fb
Igor Kirpinev <kirpinev@yahoo.com>
2020-10-16 20:19:35 +0300

    Обновил статику, настроил CSP

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
+const helmet = require("helmet");
 
 const app = express();
 const PORT = 3000;
+
+app.use(helmet());
 
 app.use('/', express.static(path.join(__dirname, 'heroku')));
 
